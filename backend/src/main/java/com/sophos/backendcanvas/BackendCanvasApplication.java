aed36bf65d0b147700c8874fae3454b4eae86db6
Jrojas <jorgerojas@seti.com.co>
2022-01-16 15:07:11 -0500

    carga inicial proyecto tareas app, tanto frontend como backend

---
+++ backend/src/main/java/com/sophos/backendcanvas/BackendCanvasApplication.java
@@ -0,0 +1,13 @@
+package com.sophos.backendcanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCanvasApplication.class, args);
+	}
+
+}
