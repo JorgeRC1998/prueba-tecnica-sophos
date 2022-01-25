4b3a2a46f7475de7c116bc4fec0347298cc81370
Jrojas <jorgerojas@seti.com.co>
2022-01-24 22:36:07 -0500

    Se reorganiza carga inicial frontend

--- frontend/FrontendCanvas/README.md
+++ frontend/FrontendCanvas/README.md
@@ -1,4 +1,4 @@
-# TareasApp
+# FrontendCanvas
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.3.
 
--- frontend/FrontendCanvas/src/app/app.component.spec.ts
+++ frontend/FrontendCanvas/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tareas-app'`, () => {
+  it(`should have as title 'FrontendCanvas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tareas-app');
+    expect(app.title).toEqual('FrontendCanvas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tareas-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FrontendCanvas app is running!');
   });
 });
