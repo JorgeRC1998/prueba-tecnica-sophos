618858229fcba9d3d44dac4159dd758b55e02a5b
Jrojas <jorgerojas@seti.com.co>
2022-01-25 19:49:30 -0500

    Pantalla inicial con el toolbar de la aplicacion

--- frontend/FrontendCanvas/src/app/app.component.ts
+++ frontend/FrontendCanvas/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontendCanvas';
+  title = 'Aplicación Tareas';
   public isAuthenticated = false;
 
   public logout(): void {
